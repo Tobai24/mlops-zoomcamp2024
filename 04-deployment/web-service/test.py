acc06ab03c6c30a9d35834d5eb9fcf58a2592b27
Akinmuleya Oluwatobi <oluwatobiiyanuoluwa24@gmail.com>
2024-06-13 15:30:28 +0000

    week 4 first commit

---
+++ 04-deployment/web-service/test.py
@@ -0,0 +1,13 @@
+import requests
+
+ride = {
+    "PULocationID":10,
+    "DOLocationID":50,
+    "trip_distance": 50 
+}
+
+url = "http://localhost:9694/predict"
+response = requests.post(url, json=ride)
+
+
+print(response.json())
